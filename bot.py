1af5e49656724a08a4284b343b304d08954cc197
vincentattafynn <vincentattafynn@gmail.com>
2023-10-25 07:52:01 +0000

    Fixed issue with previous commit. The bot works fine now

--- bot.py
+++ bot.py
@@ -1,13 +1,12 @@
 from decouple import config
 from telegram import Update
 from telegram.ext import Application, CommandHandler, filters, ContextTypes
-import telebot
 import time 
 import requests
 
 BOT_TOKEN = config('BOT_TOKEN')
+URL = "https://bible-api.com/"
 
-bot = telebot.TeleBot(BOT_TOKEN)
 
 #basic intro when the user enters hits start
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -23,12 +22,39 @@ async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
         greeting = "Hello, {update.message.chat.id} good evening"
     await update.message.reply_text(f"{greeting}, How are you today? I am a bot that gives you any Bible verse you want")
 
+#responses
+def handle_response(text: str, update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
+    processed: str = text.lower()
+
+    if str in processed:
+        response = requests.get(URL + processed)
+        response.raise_for_status()
+        response_json = response.json()
+        return response_json
+
 #functions that gets verse the user wants and extracts its for them
 async def verse_getter(verse: str) -> str:
-    verse: str = verse.lower() 
-    url = "https://bible-api.com/" + verse
-    response = requests.get(url)
-    response_json = response.json()
-    await verse.message.reply_text(f"{response_json['text']}")
+    verse = Update.message.text.split('/verse_getter ')[1]
+    verse = verse.lower()  # Remove the duplicate variable assignment
+    print("What verse do you want")
+    # Use try-except to handle exceptions when making HTTP requests
+    try:
+        response = requests.get(URL + verse)
+        response.raise_for_status()  # Check for HTTP errors
+        response_json = response.json()
+        await Update.message.reply_text(f"{response_json['text']}")
+    except requests.exceptions.RequestException as e:
+        # Handle exceptions (e.g., network errors, invalid URLs, etc.)
+        print(f"Error: {e}")
+  
+ 
+if __name__ == '__main__':
+    print("Bot starting...")
+    app = Application.builder().token(BOT_TOKEN).build()
 
-bot.infinity_polling()
+    #commands
+    app.add_handler(CommandHandler('start',start_command))
+    app.add_handler(CommandHandler('verse_getter',handle_response))
+    
+    print("Polling")
+    app.run_polling(poll_interval=3)
